83cf6c34e01a8bac2b60e1d7bbc99dbb8186cf6d
Luis Lopez <lluis6596@gmail.com>
2026-05-09 12:15:11 -0600

    Eliminar los manejadores de eventos de teclado.

--- game.js
+++ game.js
@@ -9,13 +9,7 @@ const H = 600;
 const keys = {};
 const justPressed = {};
 
-window.addEventListener('keydown', e => {
-  keys[e.code] = true;
-  justPressed[e.code] = true;
-});
-window.addEventListener('keyup', e => {
-  keys[e.code] = false;
-});
+
 
 function pressed(code) {
   const val = justPressed[code];
